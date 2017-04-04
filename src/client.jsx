985d680ce4ca76e0407bd049fe17f5ef7f9069f1
Michael Yegorov <lerayne@gmail.com>
2017-04-04 16:38:21 +0300

    make basic request

--- src/client.jsx
+++ src/client.jsx
@@ -3,6 +3,7 @@
  */
 
 import 'babel-polyfill'
+import 'isomorphic-fetch'
 import React      from 'react'
 import {render}   from 'react-dom'
 
--- src/shared/containers/MainView/index.jsx
+++ src/shared/containers/MainView/index.jsx
@@ -3,11 +3,12 @@
  */
 
 import React, {Component} from 'react'
+import url from 'url'
 import {clientID, mainURL} from '../../../../config'
 
 function LoginPage(){
     return <div>
-        <a href={`https://api.instagram.com/oauth/authorize/?client_id=${clientID}&redirect_uri=${encodeURIComponent(mainURL+'/auth')}&response_type=code`}>Login</a>
+        <a href={`https://api.instagram.com/oauth/authorize/?client_id=${clientID}&redirect_uri=${encodeURIComponent(mainURL+'/auth')}&response_type=code&scope=follower_list`}>Login</a>
     </div>
 }
 
@@ -18,6 +19,24 @@ export default class MainView extends Component {
     }
 
     render(){
-        return <LoginPage />
+
+        const loc = url.parse(url.format(window.location), true)
+
+        if (loc.query.token) {
+            return <div>
+                <button onClick={() => this.requestSubscriptions(loc.query.token)}>Subs</button>
+                <div>
+
+                </div>
+            </div>
+        } else {
+            return <LoginPage />
+        }
+    }
+
+    requestSubscriptions(token){
+        fetch('https://api.instagram.com/v1/users/self/follows?access_token=' + token, {
+            mode:'no-cors'
+        }).then(resp => resp.json()).then(json => console.log(json))
     }
 }
